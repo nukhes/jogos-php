4c079774e2f3945fff19bcc1e9be9fb6c42e0f37
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-08-29 16:05:57 -0300

    Update topo.php

--- topo.php
+++ topo.php
@@ -4,8 +4,11 @@
         if (empty($_SESSION['user'])) {
             echo "<a href='./user_login_form.php'><span style='font-size: 12pt;'>Entrar</span></a>";
         } else {
-            echo "Olá <strong>" + $_SESSION['nome'] + "</strong>.";
-            echo "Sair";
+            echo "<div>";
+            echo "Olá <strong>" . $_SESSION['nome'] . "</strong> ";
+            echo "";
+            echo "</div>";
         }  
     ?>
+    <a href='#' onclick="() => { function sair() { setCookie('PHPSESSID', 'value', 0); }}">Sair</a>
 </header>
